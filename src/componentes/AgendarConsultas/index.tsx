b8230be3f88ab977c7c064716cc7f8df3866e27d
Thaistasseo24 <tasseot@gmail.com>
2023-05-16 19:47:50 -0400

    primeiro envio

---
+++ src/componentes/AgendarConsultas/index.tsx
@@ -0,0 +1,5 @@
+export default function AgendarConsultas(){
+    return (
+        <div>Agendar Consultas</div>
+    );
+}
---
+++ src/componentes/CadastrarMedicos/index.tsx
@@ -0,0 +1,5 @@
+export default function CadastrarMedicos(){
+    return (
+        <div>Cadastrar Medicos</div>
+    );
+}
